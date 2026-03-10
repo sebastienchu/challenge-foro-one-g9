05b771e2f549aab9bec28ea25531b5d8d0c41d4a
sebastian villarroel <sebastianalvarez12121@gmail.com>
2026-03-10 13:07:34 -0500

    Create PageResponse.java

---
+++ blog_back/src/main/java-alura-blog/dominio/common/PageResponse.java
@@ -0,0 +1,28 @@
+package alura.blog.dominio.common;
+
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public class PageResponse<T> {
+
+    private final List<T> content;
+    private final int page;
+    private final int size;
+    private final long totalElements;
+    private final int totalPages;
+
+    public PageResponse(Page<T> pageData) {
+        this.content = pageData.getContent();
+        this.page = pageData.getNumber();
+        this.size = pageData.getSize();
+        this.totalElements = pageData.getTotalElements();
+        this.totalPages = pageData.getTotalPages();
+    }
+
+    public List<T> getContent() { return content; }
+    public int getPage() { return page; }
+    public int getSize() { return size; }
+    public long getTotalElements() { return totalElements; }
+    public int getTotalPages() { return totalPages; }
+}
