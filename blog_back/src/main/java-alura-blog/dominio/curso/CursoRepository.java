5f1c2af840ff3eee20e89e857416d70bdd80a882
sebastian villarroel <sebastianalvarez12121@gmail.com>
2026-03-10 13:08:54 -0500

    Create CursoRepository.java

---
+++ blog_back/src/main/java-alura-blog/dominio/curso/CursoRepository.java
@@ -0,0 +1,11 @@
+package alura.blog.dominio.curso;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+    Optional<Curso> findByNombreVisible(String nombreVisible);
+}
