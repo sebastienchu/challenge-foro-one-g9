21789404cc2c83b3ad9ed0204794d5b12329e292
sebastian villarroel <sebastianalvarez12121@gmail.com>
2026-03-10 13:27:56 -0500

    Create AutenticacionService.java

---
+++ blog_back/src/main/java-alura-blog/infra/security/AutenticacionService.java
@@ -0,0 +1,24 @@
+package alura.blog.infra.security;
+
+import alura.blog.dominio.usuario.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AutenticacionService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // Este método es llamado por Spring Security para obtener los datos del usuario
+    @Override
+    @Cacheable("usuarios")
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+    }
+}
